73e95594bcce1d69580266922b1b2d7e995bff4b
Bruce <bruce@playz.rocks>
2016-06-15 21:54:50 +0200

    Initialize Servos.

--- BotOne.ino
+++ BotOne.ino
@@ -1,12 +1,28 @@
-#include "Arduino.h"
+
+#include <Arduino.h>
+#include <Servo.h>
+
+
+const int motorServoPin = 9;
+const int steeringServoPin = 10;
+
+Servo motorServo;
+Servo steeringServo;
+
+
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
-// Add your initialization code here
+	motorServo.attach(motorServoPin);
+	steeringServo.attach(steeringServoPin);
+
+	motorServo.write(90);
+	steeringServo.write(90);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-//Add your repeated code here
+
 }
