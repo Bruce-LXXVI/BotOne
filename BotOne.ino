592842ceeeebc7819b44b4a7f91afbc12cd55fb7
Bruce <bruce@playz.rocks>
2016-06-16 23:34:07 +0200

    First try. Go forward for 5 sec.

--- BotOne.ino
+++ BotOne.ino
@@ -5,24 +5,50 @@
 
 const int motorServoPin = 9;
 const int steeringServoPin = 10;
+const int ledPin = 13;
+
+
+// Kalibration
+const int motorOffset = 1;
+const int steeringOffset = 3;
 
 Servo motorServo;
 Servo steeringServo;
 
+unsigned long time = 0;
 
 
 //The setup function is called once at startup of the sketch
 void setup()
 {
-	motorServo.attach(motorServoPin);
-	steeringServo.attach(steeringServoPin);
+	pinMode(ledPin, OUTPUT);
 
-	motorServo.write(90);
-	steeringServo.write(90);
+	motorServo.attach(motorServoPin);
+	motorServo.write(90 + motorOffset);
+
+	steeringServo.attach(steeringServoPin);
+	steeringServo.write(90 + steeringOffset);
+
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
+	if(time == 0) time=millis();
+
+
+	// Nach 5 Sekunden anhalten
+	if( (millis() - time) > 5000 ) {
+		motorServo.write(90 + motorOffset);
+		steeringServo.write(90 + steeringOffset);
+		digitalWrite(ledPin, HIGH);
+	} else {
+		// <90 rechts / >90 links
+		steeringServo.write(80 + steeringOffset);
+
+		// <90 vorwärts / >90 rückwärts
+		motorServo.write(80 + motorOffset);
+	}
+
 
 }
